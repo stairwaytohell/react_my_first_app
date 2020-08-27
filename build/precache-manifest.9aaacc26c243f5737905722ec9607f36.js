34bddeb5eacf52330cc556b1077130d7a6d318b8
Marzet <marzet.rock@gmail.com>
2020-08-27 21:23:36 +0300

    add build

---
+++ build/precache-manifest.9aaacc26c243f5737905722ec9607f36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972e25c201cfdf71be08a80ef038e76d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f584356d552e90fea42",
+    "url": "/static/css/main.de210556.chunk.css"
+  },
+  {
+    "revision": "bf2c896f6c29cbf87345",
+    "url": "/static/js/2.6e026e61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e026e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f584356d552e90fea42",
+    "url": "/static/js/main.c1182d60.chunk.js"
+  },
+  {
+    "revision": "b15679bb7b1ef7f942a3",
+    "url": "/static/js/runtime-main.bd7de267.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto.11eabca2.ttf"
+  }
+]);
