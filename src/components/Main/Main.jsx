2c128e4a5fbf7a8d89973409d899ef1fb9af096a
Marzet <marzet.rock@gmail.com>
2020-08-28 03:11:15 +0300

    ...

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-//import s from './Main.module.scss';
+import s from './Main.module.scss';
 
 const Main = () => {
 	return (
 		<main>
-
+			<div className={s.banner}></div>
+			<div className={s.top}>
+				<div className={s.ava}>
+					<a href='#profile'>
+						<img src='https://images.unsplash.com/photo-1565192060820-36811234b577' alt='ava'>
+						</img>
+					</a>
+				</div>
+				<div className={s.menu}>
+					<li><a href='#timeline' className={s.active}>Timeline</a></li>
+					<li><a href='#about'>About</a></li>
+					<li><a href='#photos'>Photos</a></li>
+					<li><a href='#friends'>Friends</a></li>
+					<li><a href='#more'>More</a></li>
+				</div>
+			</div>
 		</main>
 	);
 }
